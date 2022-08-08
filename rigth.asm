1e164e58a03a3fcdd0dd70a23460735fa864a415
sky line <32512284+linesky@users.noreply.github.com>
2022-08-08 15:28:53 +0100

    shift bits rigth sample

---
+++ rigth.asm
@@ -0,0 +1,15 @@
+
+
+#https://www.cs.cornell.edu/courses/cs3410/2019sp/riscv/interpreter/#
+
+addi a0,x0,1024
+addi a1,x0,1
+addi a2,x0,8
+addi a3,a3,0
+again:
+sw a0,0(a3)
+srl a0,a0,a1
+addi a3,a3,4
+addi a2,a2,-1
+bne a2,x0,again
+fill0:
